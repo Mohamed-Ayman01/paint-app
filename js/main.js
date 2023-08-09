b262e4922c6e88756446b2f4d6505c2f3fd91836
Mohamed-Ayman01 <lord39597@gmail.com>
2023-08-09 20:27:21 +0400

    created draw feature + settings

--- js/main.js
+++ js/main.js
@@ -2,11 +2,71 @@ let canvas = document.querySelector("canvas");
 let ctx = canvas.getContext("2d");
 let toolBar = document.querySelector(".tool-bar");
 
-// adjust canvas size
-canvas.height = window.innerHeight - toolBar.clientHeight;
-canvas.width = window.innerWidth;
+//! adjust canvas size
 
-window.addEventListener("resize", () => {
+function adjustCanvasSize() {
   canvas.height = window.innerHeight - toolBar.clientHeight;
   canvas.width = window.innerWidth;
+}
+adjustCanvasSize();
+
+window.addEventListener("resize", adjustCanvasSize);
+
+//! canvas clear feature
+
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+let clearBtn = document.querySelector(".tools button.clear");
+clearBtn.addEventListener("click", clearCanvas);
+
+//! drawing feature
+
+function enableDrawing() {
+  let isMouseDown;
+
+  ctx.lineCap = "round";
+
+  function draw(e) {
+    if (!isMouseDown) return;
+
+    // just to draw
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.moveTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+
+    // to make drawing line smooth
+    ctx.beginPath();
+    ctx.moveTo(e.offsetX, e.offsetY);
+  }
+
+  canvas.addEventListener("mousedown", (e) => {
+    isMouseDown = true;
+    draw(e);
+  });
+
+  canvas.addEventListener("mouseup", () => {
+    isMouseDown = false;
+    ctx.beginPath();
+  });
+
+  canvas.addEventListener("mouseout", () => {
+    isMouseDown = false;
+  });
+
+  canvas.addEventListener("mousemove", draw);
+}
+enableDrawing();
+
+//! Set pen size and color
+let colorInput = document.querySelector(`.setting input[type="color"]`);
+let sizeInput = document.querySelector(`.setting input[type="number"]`);
+
+colorInput.addEventListener("change", () => {
+  ctx.strokeStyle = colorInput.value;
+});
+
+sizeInput.addEventListener("change", () => {
+  ctx.lineWidth = sizeInput.value;
 });
